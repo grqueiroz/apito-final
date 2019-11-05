3b94554a05decfcf8aaa5ff15a65d307c56be98e
Gabriel Rates de Queiroz <grqueiroz3@gmail.com>
2019-11-05 19:51:23 -0300

    Improving db config

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 module.exports = {
 
-    dbConnectionString:  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0-gqzqm.mongodb.net/apito-final?retryWrites=true&w=majority`,
+    dbConnectionString:  process.env.MONGODB_URL,
     port: process.env.PORT
 
 }
