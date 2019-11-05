4adadf55fd7ff965fc5ce7ad297792a9b8d976ab
Gabriel Rates de Queiroz <gabriel.queiroz@ifood.com.br>
2019-11-05 00:00:26 -0300

    Standings model

---
+++ components/standings/model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const standingsSchema = new Schema({
+    position: Number,
+    team: String,
+    points: Number,
+    played: Number,
+    wins: Number,
+    draws: Number,
+    losses: Number,
+    goalsFor: Number,
+    goalsAgainst: Number,
+    form: [
+        Number,
+        Number,
+        Number,
+        Number,
+        Number        
+    ],
+    baseRound: Number
+});
+
+const Standings = mongoose.model('Standings', standingsSchema);
+
+module.exports = Standings;
